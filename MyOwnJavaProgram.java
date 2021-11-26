a46c69ca56546faf678cef063c562e737246cceb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-26 15:44:14 -0800

    compile and run MyOwnJavaProgram

--- MyOwnJavaProgram.java
+++ MyOwnJavaProgram.java
@@ -6,7 +6,7 @@
 public class MyOwnJavaProgram {
     // In the main method, implement the code to print your name, age, and hometown on different lines.
     public static void main(String[] args) {
-        System.out.println("Hello, my name if Virgilio D. Cabading Jr.");
+        System.out.println("Hello, my name is Virgilio D. Cabading Jr.");
         System.out.println("I am 43 years old.");
         System.out.println("My hometown is Houston, Texas");
     }
