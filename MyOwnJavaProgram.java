cdce7f43bdf4bda43dfab0c3cd11dbe5a1e5ef54
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-26 15:33:16 -0800

    created MyOwnJavaProgram Class and main executable method

--- MyOwnJavaProgram.java
+++ MyOwnJavaProgram.java
@@ -2,3 +2,9 @@
 // COding Dojo > Java Stack > Fundamentals: My Own Java Program
 // By:  Virgilio D. Cabading Jr.    Created at: November 26, 2021 1530
 // /////////////////////////////////////////////////////////////////
+
+public class MyOwnJavaProgram {
+    public static void main(String[] args) {
+        
+    }
+}
